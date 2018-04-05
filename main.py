26ffce4ad6351fdff040445c6f7bd8f7ed127527
Belal <billy.fcih@yahoo.com>
2018-04-06 00:50:57 +0200

    Finish building and training the model

--- main.py
+++ main.py
@@ -23,3 +23,16 @@ response = train['cnt']
 
 # Get the number of features
 n_cols = predictors.shape[1]
+
+# Build the model
+model = Sequential()
+model.add(Dense(10, activation='relu', input_shape=(n_cols,)))
+model.add(Dense(1))
+
+# Stop the training after the loss increases 3 times in a row
+early_stopping_monitor = EarlyStopping(patience=3)
+
+# Compile, train and save the model
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+model.fit(predictors, response, validation_split=0.2, verbose=1, epochs=50, callbacks=[early_stopping_monitor])
+model.save('model.h5')
