0c7e49ab999eeacb9b7c06243cd5a8cf56537c8e
Belal <billy.fcih@yahoo.com>
2018-04-06 00:49:34 +0200

    Prepare the data for training

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import numpy as np
+import pandas as pd
+import tensorflow as tf
+from keras import backend as k
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.models import load_model
+from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import SGD, Adam, RMSprop
+
+# For reproducibility
+np.random.seed(123)
+
+# Read the data
+train = pd.read_csv('Dataset/reg_train.csv', index_col=['instant', 'dteday'])
+test = pd.read_csv('Dataset/reg_test.csv', index_col=['instant', 'dteday'])
+
+# Initialize the predictors and the response
+predictors = train.drop(['cnt'], axis=1)
+response = train['cnt']
+
+# Get the number of features
+n_cols = predictors.shape[1]
