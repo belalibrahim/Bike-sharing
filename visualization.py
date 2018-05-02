b8062d132aae6679f96b2f8f00f67eedfeee7654
Belal <billy.fcih@yahoo.com>
2018-05-03 01:20:25 +0200

    Visualize the distribution for each feature

---
+++ visualization.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Read the data
+train = pd.read_csv('Dataset/reg_train.csv', index_col=['instant', 'dteday'])
+test = pd.read_csv('Dataset/reg_test.csv', index_col=['instant', 'dteday'])
+
+# Visualize the distribution for each feature
+for i in range(len(train.columns)):
+
+    sns.distplot(train.iloc[:, i])
+    plt.show()
